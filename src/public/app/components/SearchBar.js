2a888d58be501dd6ac96357c106423c86457613f
Anthony Greenheck <aemgreenheck@gmail.com>
2017-09-24 14:30:35 -0400

    [added] routes

--- src/public/app/components/SearchBar.js
+++ src/public/app/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import superagent from 'superagent'
 
 class SearchBar extends React.Component {
 
@@ -7,13 +8,34 @@ class SearchBar extends React.Component {
     this.state = {
       pictures: ['default'],
     }
+    this.test = this.test.bind(this);
+  }
+
+  test () {
+    superagent
+        .get('/test')
+        // .query(null)
+        // .set('Accept', 'aplication/json')
+        .end((err, response) => {
+           if(err) {
+             response.json({
+               confirmation: 'fail',
+               messgage: err.message
+             })
+             return
+           }
+           console.log('RESPONSE: ' + JSON.stringify(response.body.data.items))
+          //  this.props.feedFetched(response.body.data.items)
+           this.setState({
+             feed: response.body.data.items
+           })
+        })
   }
 
   render() {
     return (
-      <div>
-        <h3>searchBar</h3>
-        <button>search</button>
+      <div className="searchBar">
+        <button onClick={this.test}>search</button>
         <span>{this.state.pictures}</span>
       </div>
     )
