32561bb3ba04e87cb920bef3c81605a8401d7558
Anthony Greenheck <aemgreenheck@gmail.com>
2017-09-24 21:13:03 -0400

    [added] instagram retrieval

--- src/public/app/components/SearchBar.js
+++ src/public/app/components/SearchBar.js
@@ -6,37 +6,56 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pictures: ['default'],
+      pictures: '' ,
+      movies: 'test',
+      feed: []
     }
     this.test = this.test.bind(this);
   }
 
-  test () {
-    superagent
-        .get('/test')
-        // .query(null)
-        // .set('Accept', 'aplication/json')
-        .end((err, response) => {
-           if(err) {
-             response.json({
-               confirmation: 'fail',
-               messgage: err.message
-             })
-             return
-           }
-           console.log('RESPONSE: ' + JSON.stringify(response.body.data.items))
-          //  this.props.feedFetched(response.body.data.items)
-           this.setState({
-             feed: response.body.data.items
-           })
-        })
+  // test () {
+  //   superagent
+  //       .get('/test')
+  //       // .query(null)
+  //       // .set('Accept', 'aplication/json')
+  //       .end((err, response) => {
+  //          if(err) {
+  //            response.json({
+  //              confirmation: 'fail',
+  //              messgage: err.message
+  //            })
+  //            return
+  //          }
+  //          console.log('RESPONSE: ' + JSON.stringify(response.body.data.items))
+  //         //  this.props.feedFetched(response.body.data.items)
+  //          this.setState({
+  //            feed: response.body.data.items
+  //          })
+  //       })
+  // }
+
+test() {
+    var url = 'https://www.instagram.com/tonygreenheck/media/';
+    superagent.get(url).then((response) => {
+      this.setState({
+        feed: response.body.items
+      })
+      console.log('woot', this.state.feed);
+    });
   }
 
   render() {
+
     return (
       <div className="searchBar">
         <button onClick={this.test}>search</button>
-        <span>{this.state.pictures}</span>
+        {this.state.feed.map((img, idx) => {
+          return (
+            <div key={idx}>
+              <img src={img.images.standard_resolution.url} />
+            </div>
+          )
+        })}
       </div>
     )
   }
