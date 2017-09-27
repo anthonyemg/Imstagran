966fd98bffeb23553b5612c47c0e6a5823e103be
Anthony Greenheck <aemgreenheck@gmail.com>
2017-09-27 16:45:28 -0400

    [changed] searchbar buttons to proper text and size

--- src/public/app/components/SearchBar.js
+++ src/public/app/components/SearchBar.js
@@ -8,24 +8,27 @@ class SearchBar extends React.Component {
 
   render () {
     return (
-      <div className='searchBar'>
-        <div className='searchBar-title'>
-          <i className='fa fa-instagram fa-2x' />
-          <span>imstagran</span>
-        </div>
-        <input
-          type='text'
-          placeholder='Search'
-          value={this.props.searchUsername}
-          onChange={this.props.handleUserChange}
-          onKeyPress={this.props.handleKeyPress}
-        />
-        <div className='searchBar-buttons'>
-          {/* <i className="fa fa-circle-thin fa-lg" />
-          <i className="fa fa-heart-o fa-lg" />
-          <i className="fa fa-user-o fa-lg" /> */}
-          <button>Get the app</button>
-          <span>Sign up | Log in </span>
+      <div className='searchBar-container'>
+        <div className='searchBar'>
+          <div className='searchBar-title'>
+            <i className='fa fa-instagram fa-2x' />
+            <span>imstagran</span>
+          </div>
+          <input
+            type='text'
+            placeholder='Search'
+            value={this.props.searchUsername}
+            onChange={this.props.handleUserChange}
+            onKeyPress={this.props.handleKeyPress}
+          />
+          <div className='searchBar-buttons'>
+            <button>Get the app</button>
+            <div className='searchBar-buttonsSignUpLogIn'>
+              <span>Sign up</span>
+              <span style={{color: 'rgb(199,199,199)', margin: '0px 7px'}}>|</span>
+              <span>Log in</span>
+            </div>
+          </div>
         </div>
       </div>
     )
