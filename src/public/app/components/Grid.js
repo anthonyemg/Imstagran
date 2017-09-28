b478df9332273e8967bdb54aaf17b46412a68c39
Anthony Greenheck <aemgreenheck@gmail.com>
2017-09-27 21:20:32 -0400

    [tweaked] grid pphoto destortion

--- src/public/app/components/Grid.js
+++ src/public/app/components/Grid.js
@@ -2,17 +2,26 @@ import React from 'react';
 
 class Grid extends React.Component {
 
-  constructor(props) {
-    super(props)
+  constructor  (props) {
+    super (props)
   }
 
   render () {
     return (
-      <div className='grid'>
+      <div className = 'grid'>
         {this.props.feed ?
-          <div className="grid-wrapper">
+          <div className = "grid-wrapper">
             {this.props.feed.map((img, idx) =>  (
-              <img src={img.images.standard_resolution.url} width='100%' height='100%' key={idx} />
+
+              <div className = 'grid-componentContainer' key = {idx}>
+                <img
+                  className = 'grid-component'
+                  src = {img.images.standard_resolution.url}
+                  width = {img.images.standard_resolution.width > img.images.standard_resolution.height ? 'auto' : '100%'}
+                  height = {img.images.standard_resolution.height > img.images.standard_resolution.width ? 'auto' : '100%'}
+                />
+              </div>
+
             ))}
           </div>
           : <h3>search a user ^</h3>
